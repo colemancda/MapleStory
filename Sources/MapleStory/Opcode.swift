923222f12ea4cf0b09468c8b8cdb1cb845061528
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-14 08:32:41 -0800

    Add `Opcode`

---
+++ Sources/MapleStory/Opcode.swift
@@ -0,0 +1,38 @@
+//
+//  PacketOpcode.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 12/4/22.
+//
+
+/// MapleStory Opcode
+public struct Opcode: RawRepresentable, Equatable, Hashable, Codable {
+    
+    public let rawValue: UInt16
+    
+    public init(rawValue: UInt16) {
+        self.rawValue = rawValue
+    }
+}
+
+// MARK: - ExpressibleByIntegerLiteral
+
+extension Opcode: ExpressibleByIntegerLiteral {
+    
+    public init(integerLiteral value: UInt16) {
+        self.init(rawValue: value)
+    }
+}
+
+// MARK: - CustomStringConvertible
+
+extension Opcode: CustomStringConvertible, CustomDebugStringConvertible {
+    
+    public var description: String {
+        rawValue.description
+    }
+    
+    public var debugDescription: String {
+        description
+    }
+}
