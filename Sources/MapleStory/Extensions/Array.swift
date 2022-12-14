0ac0456f108348d219f39347bd99f33403c04246
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-14 08:33:05 -0800

    Add extensions

---
+++ Sources/MapleStory/Extensions/Array.swift
@@ -0,0 +1,15 @@
+//
+//  Array.swift
+//
+//
+//  Created by Alsey Coleman Miller on 12/14/22.
+//
+
+internal extension Array {
+    
+    mutating func popFirst() -> Element? {
+        guard let first = self.first else { return nil }
+        self.removeFirst()
+        return first
+    }
+}
