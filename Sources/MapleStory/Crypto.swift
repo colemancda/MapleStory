2bb319de23a1c3635ad07962c4c4abf281f2780d
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-16 08:42:52 -0800

    Updated `Crypto.Maple.encrypt()`

--- Sources/MapleStory/Crypto.swift
+++ Sources/MapleStory/Crypto.swift
@@ -17,76 +17,76 @@ internal enum Crypto {
     enum Maple {
         
         static func encrypt(_ data: inout Data) {
-            let nbytes = data.count
-            var a, c: UInt8
             
-            for _ in 0..<3 {
+            for j in 0 ..< 6 {
+                var remember: UInt8 = 0
+                var dataLength = UInt8(data.count & 0xff)
                 
-                a = 0
-
-                for j in (1...nbytes).reversed() {
-                    c = data[nbytes - j]
-                    c = c.rotatedLeft(by: 3)
-                    c = c &+ UInt8(j)
-                    c ^= a
-                    a = c
-                    c = a.rotatedRight(by: j)
-                    c ^= 0xFF
-                    c &+= 0x48
-                    data[nbytes - j] = c
-                }
-
-                a = 0
-
-                for j in (1...nbytes).reversed() {
-                    c = data[j - 1]
-                    c = c.rotatedLeft(by: 4)
-                    c = UInt8(Int(c) + j)
-                    c ^= a
-                    a = c
-                    c ^= 0x13
-                    c = c.rotatedRight(by: 3)
-                    data[j - 1] = c
+                if j % 2 == 0 {
+                    for i in 0..<data.count {
+                        var cur = data[i]
+                        cur = cur.rollLeft(3)
+                        cur += dataLength
+                        cur ^= remember
+                        remember = cur
+                        cur = cur.rollRight(Int(dataLength & 0xff))
+                        cur = UInt8((~cur) & 0xFF)
+                        cur += 0x48
+                        dataLength -= 1
+                        data[i] = cur
+                    }
+                } else {
+                    for i in (0..<data.count).reversed() {
+                        var cur = data[i]
+                        cur = cur.rollLeft(4)
+                        cur += dataLength
+                        cur ^= remember
+                        remember = cur
+                        cur ^= 0x13
+                        cur = cur.rollRight(3)
+                        dataLength -= 1
+                        data[i] = cur
+                    }
                 }
             }
         }
         
         static func decrypt(_ data: inout Data) {
-            let nbytes = data.count
-            var a, b, c: UInt8
-
-             for _ in 0 ..< 3 {
-                 a = 0
-                 b = 0
-
-                 for j in stride(from: nbytes, to: 0, by: -1) {
-                     c = data[j - 1]
-                     c = c.rotatedLeft(by: 3)
-                     c ^= 0x13
-                     a = c
-                     c ^= b
-                     c = UInt8(Int(c) - j)
-                     c = c.rotatedRight(by: -4)
-                     b = a
-                     data[j - 1] = c
-                 }
-
-                 a = 0
-                 b = 0
-
-                 for j in stride(from: nbytes, to: 0, by: -1) {
-                     c = data[nbytes - j]
-                     c -= 0x48
-                     c ^= 0xFF
-                     c = c.rotatedLeft(by: j)
-                     a = c
-                     c ^= b
-                     c = UInt8(Int(c) - j)
-                     c = c.rotatedRight(by: -3)
-                     b = a
-                     data[nbytes - j] = c
-                 }
-             }
+            
+            for j in 1...6 {
+                var remember: UInt8 = 0
+                var nextRemember: UInt8 = 0
+                var dataLength = UInt8(data.count & 0xff)
+                
+                if j % 2 == 0 {
+                    for i in 0..<data.count {
+                        var cur = data[i]
+                        cur -= 0x48
+                        cur = ~cur
+                        cur = cur << UInt8(dataLength & 0xff)
+                        nextRemember = cur
+                        cur ^= remember
+                        remember = nextRemember
+                        cur -= dataLength
+                        cur = cur >> 3
+                        data[i] = cur
+                        dataLength -= 1
+                    }
+                } else {
+                    for i in (0..<data.count).reversed() {
+                        var cur = data[i]
+                        cur = cur << 3
+                        cur ^= 0x13
+                        nextRemember = cur
+                        cur ^= remember
+                        remember = nextRemember
+                        cur -= dataLength
+                        cur = cur >> 4
+                        data[i] = cur
+                        dataLength -= 1
+                    }
+                }
+            }
         }
     }
 }
--- Sources/MapleStory/Extensions/Integer.swift
+++ Sources/MapleStory/Extensions/Integer.swift
@@ -61,25 +61,21 @@ internal extension UInt64 {
 
 internal extension UInt8 {
     
-    /// Performs a left rotation, shifting the bits in self to the left by n places and wrapping around any bits that are shifted past the leftmost bit.
-    mutating func rotatedLeft(by n: Int) -> UInt8 {
-        var msb: UInt8 = 0
-        for _ in 0 ..< n {
-            msb = (self & 0x80) != 0 ? 1 : 0
-            self <<= 1
-            self |= msb
-        }
-        return self
+    /*
+     Takes a byte value and an `Int` value `count`, and returns the result of shifting the bits in in to the left by count positions, wrapping around to the right if necessary. The count parameter is first taken modulo 8 to ensure that it is in the range 0 to 7. The value of in is first cast to an int and masked with 0xFF (which is a binary number with 8 ones, representing the lower 8 bits) to ensure that the operation is performed on a positive integer. The value is then shifted left by count positions, and the result is masked with 0xFF to ensure that it fits in a single byte. Finally, the result is cast back to a byte and returned.
+     */
+    func rollLeft(_ count: Int) -> UInt8 {
+        var tmp = Int(self & 0xFF)
+        tmp = tmp << (count % 8)
+        return UInt8((tmp & 0xFF) | (tmp >> 8))
     }
     
-    /// Performs a left rotation, shifting the bits in self to the right by n places and wrapping around any bits that are shifted past the rightmost bit.
-    mutating func rotatedRight(by n: Int) -> UInt8 {
-        var lsb: UInt8 = 0
-        for _ in 0 ..< n {
-            lsb = (self & 1) != 0 ? 0x80 : 0
-            self >>= 1
-            self |= lsb
-        }
-        return self
+    /*
+     Shifts the bits to the right by count positions, wrapping around to the left if necessary. The value of in is first cast to an int and masked with 0xFF, then shifted left by 8 positions to fill the high-order bits. The value is then shifted right by count positions, and the result is masked with 0xFF to ensure that it fits in a single byte. Finally, the result is cast back to a byte and returned.
+     */
+    func rollRight(_ count: Int) -> UInt8 {
+        var tmp = Int(self & 0xFF)
+        tmp = (tmp << 8) >> (count % 8)
+        return UInt8((tmp & 0xFF) | (tmp >> 8))
     }
 }
