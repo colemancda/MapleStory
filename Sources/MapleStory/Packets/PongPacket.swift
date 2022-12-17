2ec9552a0575a4321f97ae35b7a80a87b709568b
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-17 13:38:58 -0800

    Add `PongPacket`

---
+++ Sources/MapleStory/Packets/PongPacket.swift
@@ -0,0 +1,15 @@
+//
+//  PongPacket.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 12/16/22.
+//
+
+import Foundation
+
+public struct PongPacket: MapleStoryPacket, Codable, Equatable, Hashable {
+    
+    public static var opcode: Opcode { 0x0012 }
+    
+    public init() { }
+}
