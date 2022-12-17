00886a7389ac0ffce1b0e7f3efdb5461bc1fc979
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-17 13:38:03 -0800

    Add `MapleStoryError`

---
+++ Sources/MapleStory/Error.swift
@@ -0,0 +1,16 @@
+//
+//  Error.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 12/17/22.
+//
+
+import Foundation
+
+/// MapleStory Error
+public enum MapleStoryError: Error {
+    
+    case invalidData(Data)
+    
+    case notAuthenticated
+}
