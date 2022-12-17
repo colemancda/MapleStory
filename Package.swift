1109582f33e60e9001f229a7bd975b8873d48a0f
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-17 13:37:53 -0800

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -3,18 +3,41 @@ import PackageDescription
 
 let package = Package(
     name: "MapleStory",
+    platforms: [
+        .macOS(.v12),
+    ],
     products: [
         .library(
             name: "MapleStory",
-            targets: ["MapleStory"]),
+            targets: ["MapleStory"]
+        ),
     ],
     dependencies: [
-        
+        .package(
+            url: "https://github.com/PureSwift/Socket",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/krzyzanowskim/CryptoSwift.git",
+            .upToNextMajor(from: "1.6.0")
+        ),
+        .package(
+            url: "https://github.com/apple/swift-argument-parser",
+            from: "1.2.0"
+        )
     ],
     targets: [
         .target(
             name: "MapleStory",
-            dependencies: ["CMapleStory"]
+            dependencies: [
+                "CMapleStory",
+                "Socket",
+                "CryptoSwift",
+                .product(
+                    name: "ArgumentParser",
+                    package: "swift-argument-parser"
+                )
+            ]
         ),
         .target(
             name: "CMapleStory",
