db89038ff2d10fa63e74f0165802e78be09695ec
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-19 10:21:23 -0800

    Updated `Opcode.description`

--- Sources/MapleStory/Opcode.swift
+++ Sources/MapleStory/Opcode.swift
@@ -29,7 +29,7 @@ extension Opcode: ExpressibleByIntegerLiteral {
 extension Opcode: CustomStringConvertible, CustomDebugStringConvertible {
     
     public var description: String {
-        rawValue.description
+        "0x" + rawValue.toHexadecimal()
     }
     
     public var debugDescription: String {
