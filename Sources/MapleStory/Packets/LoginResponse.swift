45fe22c3383d8ac0a4746d4513dbdb51cc424421
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-20 13:28:00 -0800

    Add `LoginResponse.success`

--- Sources/MapleStory/Packets/LoginResponse.swift
+++ Sources/MapleStory/Packets/LoginResponse.swift
@@ -8,9 +8,40 @@
 import Foundation
 
 /// Login Response
-public struct LoginResponse: MapleStoryPacket, Codable, Equatable, Hashable {
+public enum LoginResponse: MapleStoryPacket, Equatable, Hashable {
     
-    public static var opcode: Opcode { 0x01 }
+    public static var opcode: Opcode { 0x00 }
     
-    public init() { }
+    case success(username: String)
+}
+
+// MARK: - Encodable
+
+extension LoginResponse: Encodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case username
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case let .success(username):
+            try container.encode(username, forKey: CodingKeys.username)
+        }
+    }
+}
+
+// MARK: - MapleStoryEncodable
+
+extension LoginResponse: MapleStoryEncodable {
+    
+    public func encode(to container: MapleStoryEncodingContainer) throws {
+        switch self {
+        case let .success(username):
+            try container.encode(Data([0, 0, 0, 0, 0, 0, 0xFF, 0x6A, 1, 0, 0, 0, 0x4E]))
+            try container.encode(username, forKey: CodingKeys.username)
+            try container.encode(Data([3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xDC, 0x3D, 0x0B, 0x28, 0x64, 0xC5, 1, 8, 0, 0, 0]))
+        }
+    }
 }
