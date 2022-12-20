cfc0e0af453dd04d0c86b377a4c723576efd9282
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-20 15:25:59 -0800

    Add `ServerStatusResponse`

---
+++ Sources/MapleStory/Packets/ServerStatusResponse.swift
@@ -0,0 +1,25 @@
+//
+//  ServerStatusResponse.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 12/20/22.
+//
+
+import Foundation
+
+/// Server Status Response
+///
+/// Packet detailing a server status message.
+public enum ServerStatusResponse: UInt16, MapleStoryPacket, Codable, Equatable, Hashable {
+    
+    public static var opcode: Opcode { 0x03 }
+    
+    /// Normal
+    case normal         = 0
+    
+    /// High Usage
+    case highUsage      = 1
+    
+    /// Full
+    case full           = 2
+}
