ef4b990c5cd553bb27eb45ca0891f9a89db38098
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-21 23:07:50 -0800

    Updated unit tests

--- Tests/MapleStoryTests/LoginTests.swift
+++ Tests/MapleStoryTests/LoginTests.swift
@@ -394,4 +394,130 @@ final class LoginTests: XCTestCase {
         XCTAssertEqual(encrypted.length, packet.data.count)
         XCTAssertEqual(encrypted.data, encryptedData)
     }
+    
+    func testAllCharactersInfo() throws {
+        
+        /*
+         MaplePacketEncoder will write encrypted 08 00 00 00 01 01 00 00 00 41 64 6D 69 6E 00 00 00 00 00 00 00 00 00 00 20 4E 00 00 4E 75 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 FE 00 02 FF 7F FF 7F FF 7F FF 7F 30 75 30 75 6A 67 30 75 00 00 00 00 00 00 00 00 19 34 00 00 00 00 80 7F 3D 36 01 00 00 00 00 00 00 20 4E 00 00 01 4E 75 00 00 05 82 DE 0F 00 06 A2 2C 10 00 09 D9 D0 10 00 01 75 4B 0F 00 07 81 5B 10 00 0B 27 9D 16 00 FF FF 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01 01 00 00 00 00 00 00 00 01 00 00 00 00 00 00 00
+         MaplePacketEncoder header 2A 47 84 47
+         MaplePacketEncoder custom encrypted C6 8F 2D 05 5A CD 87 2D 0E EC AC 67 84 EE 54 EC 74 A0 B2 AD 05 63 54 C0 A1 5F CD 6B F6 38 11 B4 EA 85 B0 9E A6 0E 6D 97 2A B9 F5 A6 FB F1 26 8D B1 B8 15 4C 63 38 7B 85 8F BD A1 04 64 5D F7 56 9D 1F A3 A4 B9 97 B1 15 F7 7A 2F 6D 10 07 5C C1 FB 5A 2F 89 07 C1 23 AE 12 72 18 98 F7 73 46 0F 79 B7 3B 61 A6 DC BF 8A 7D 81 74 E5 00 AA 66 18 F8 E0 E1 23 39 DD 9B 4A 04 33 1C 0C 18 C9 6D 89 99 CA 44 E8 56 40 2D 12 CE 35 F9 47 D0 F4 78 7B 89 8B 4F 10 97 83 2A 89 0B E7 6C CD 9F C9 BA B3 A9 ED 04 07 C8 6B 92 5B 75 75 4B 50 8B F4
+         MapleAESOFB.crypt() input: C6 8F 2D 05 5A CD 87 2D 0E EC AC 67 84 EE 54 EC 74 A0 B2 AD 05 63 54 C0 A1 5F CD 6B F6 38 11 B4 EA 85 B0 9E A6 0E 6D 97 2A B9 F5 A6 FB F1 26 8D B1 B8 15 4C 63 38 7B 85 8F BD A1 04 64 5D F7 56 9D 1F A3 A4 B9 97 B1 15 F7 7A 2F 6D 10 07 5C C1 FB 5A 2F 89 07 C1 23 AE 12 72 18 98 F7 73 46 0F 79 B7 3B 61 A6 DC BF 8A 7D 81 74 E5 00 AA 66 18 F8 E0 E1 23 39 DD 9B 4A 04 33 1C 0C 18 C9 6D 89 99 CA 44 E8 56 40 2D 12 CE 35 F9 47 D0 F4 78 7B 89 8B 4F 10 97 83 2A 89 0B E7 6C CD 9F C9 BA B3 A9 ED 04 07 C8 6B 92 5B 75 75 4B 50 8B F4
+         MapleAESOFB.crypt() iv: 79 B9 EB B8
+         MapleAESOFB.crypt() output: C5 93 F9 DB 27 75 29 30 DB A1 78 6C 98 39 4B 93 C0 E9 28 60 CF 53 C2 99 5F 20 D9 11 85 4A D4 8B 81 A8 6E 67 B3 33 96 E0 5F 3F 9A 66 41 CB 86 4A 4B 33 F4 2E 2E E7 99 F2 A7 C9 31 E1 FB 45 86 34 2E 56 69 45 4E 31 4D 64 DA 00 4A 53 86 26 D9 31 50 AB 0F DA FA B9 5A 5F 35 AE 1A 04 42 65 93 24 00 D5 DF DF 22 43 8A 3A F8 69 7B E1 D9 69 2B 61 0A DE 34 66 01 82 86 0F EE 0A DC 1E 27 C0 23 E0 18 A4 5E 05 D9 CA 2F 8A 73 46 9A F7 53 A9 89 70 5A 02 5E 67 E7 E6 89 15 78 7A 5F 4F 31 05 36 82 18 E3 5C AE F7 7D 58 C5 91 23 43 DE 90 F7
+         MaplePacketEncoder AES encrypted C5 93 F9 DB 27 75 29 30 DB A1 78 6C 98 39 4B 93 C0 E9 28 60 CF 53 C2 99 5F 20 D9 11 85 4A D4 8B 81 A8 6E 67 B3 33 96 E0 5F 3F 9A 66 41 CB 86 4A 4B 33 F4 2E 2E E7 99 F2 A7 C9 31 E1 FB 45 86 34 2E 56 69 45 4E 31 4D 64 DA 00 4A 53 86 26 D9 31 50 AB 0F DA FA B9 5A 5F 35 AE 1A 04 42 65 93 24 00 D5 DF DF 22 43 8A 3A F8 69 7B E1 D9 69 2B 61 0A DE 34 66 01 82 86 0F EE 0A DC 1E 27 C0 23 E0 18 A4 5E 05 D9 CA 2F 8A 73 46 9A F7 53 A9 89 70 5A 02 5E 67 E7 E6 89 15 78 7A 5F 4F 31 05 36 82 18 E3 5C AE F7 7D 58 C5 91 23 43 DE 90 F7
+         MaplePacketEncoder output 2A 47 84 47 C5 93 F9 DB 27 75 29 30 DB A1 78 6C 98 39 4B 93 C0 E9 28 60 CF 53 C2 99 5F 20 D9 11 85 4A D4 8B 81 A8 6E 67 B3 33 96 E0 5F 3F 9A 66 41 CB 86 4A 4B 33 F4 2E 2E E7 99 F2 A7 C9 31 E1 FB 45 86 34 2E 56 69 45 4E 31 4D 64 DA 00 4A 53 86 26 D9 31 50 AB 0F DA FA B9 5A 5F 35 AE 1A 04 42 65 93 24 00 D5 DF DF 22 43 8A 3A F8 69 7B E1 D9 69 2B 61 0A DE 34 66 01 82 86 0F EE 0A DC 1E 27 C0 23 E0 18 A4 5E 05 D9 CA 2F 8A 73 46 9A F7 53 A9 89 70 5A 02 5E 67 E7 E6 89 15 78 7A 5F 4F 31 05 36 82 18 E3 5C AE F7 7D 58 C5 91 23 43 DE 90 F7
+         */
+        
+        let packetData = Data([0x08, 0x00, 0x00, 0x00, 0x01, 0x01, 0x00, 0x00, 0x00, 0x41, 0x64, 0x6D, 0x69, 0x6E, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x4E, 0x00, 0x00, 0x4E, 0x75, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFE, 0x00, 0x02, 0xFF, 0x7F, 0xFF, 0x7F, 0xFF, 0x7F, 0xFF, 0x7F, 0x30, 0x75, 0x30, 0x75, 0x6A, 0x67, 0x30, 0x75, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x19, 0x34, 0x00, 0x00, 0x00, 0x00, 0x80, 0x7F, 0x3D, 0x36, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x4E, 0x00, 0x00, 0x01, 0x4E, 0x75, 0x00, 0x00, 0x05, 0x82, 0xDE, 0x0F, 0x00, 0x06, 0xA2, 0x2C, 0x10, 0x00, 0x09, 0xD9, 0xD0, 0x10, 0x00, 0x01, 0x75, 0x4B, 0x0F, 0x00, 0x07, 0x81, 0x5B, 0x10, 0x00, 0x0B, 0x27, 0x9D, 0x16, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+        let encryptedData = Data([0x2A, 0x47, 0x84, 0x47, 0xC5, 0x93, 0xF9, 0xDB, 0x27, 0x75, 0x29, 0x30, 0xDB, 0xA1, 0x78, 0x6C, 0x98, 0x39, 0x4B, 0x93, 0xC0, 0xE9, 0x28, 0x60, 0xCF, 0x53, 0xC2, 0x99, 0x5F, 0x20, 0xD9, 0x11, 0x85, 0x4A, 0xD4, 0x8B, 0x81, 0xA8, 0x6E, 0x67, 0xB3, 0x33, 0x96, 0xE0, 0x5F, 0x3F, 0x9A, 0x66, 0x41, 0xCB, 0x86, 0x4A, 0x4B, 0x33, 0xF4, 0x2E, 0x2E, 0xE7, 0x99, 0xF2, 0xA7, 0xC9, 0x31, 0xE1, 0xFB, 0x45, 0x86, 0x34, 0x2E, 0x56, 0x69, 0x45, 0x4E, 0x31, 0x4D, 0x64, 0xDA, 0x00, 0x4A, 0x53, 0x86, 0x26, 0xD9, 0x31, 0x50, 0xAB, 0x0F, 0xDA, 0xFA, 0xB9, 0x5A, 0x5F, 0x35, 0xAE, 0x1A, 0x04, 0x42, 0x65, 0x93, 0x24, 0x00, 0xD5, 0xDF, 0xDF, 0x22, 0x43, 0x8A, 0x3A, 0xF8, 0x69, 0x7B, 0xE1, 0xD9, 0x69, 0x2B, 0x61, 0x0A, 0xDE, 0x34, 0x66, 0x01, 0x82, 0x86, 0x0F, 0xEE, 0x0A, 0xDC, 0x1E, 0x27, 0xC0, 0x23, 0xE0, 0x18, 0xA4, 0x5E, 0x05, 0xD9, 0xCA, 0x2F, 0x8A, 0x73, 0x46, 0x9A, 0xF7, 0x53, 0xA9, 0x89, 0x70, 0x5A, 0x02, 0x5E, 0x67, 0xE7, 0xE6, 0x89, 0x15, 0x78, 0x7A, 0x5F, 0x4F, 0x31, 0x05, 0x36, 0x82, 0x18, 0xE3, 0x5C, 0xAE, 0xF7, 0x7D, 0x58, 0xC5, 0x91, 0x23, 0x43, 0xDE, 0x90, 0xF7])
+        let nonce: Nonce = 0x79B9EBB8
+        
+        guard let packet = Packet(data: packetData) else {
+            XCTFail()
+            return
+        }
+        
+        let value = AllCharactersResponse.characters(world: 0, characters: [
+            MapleStory.CharacterListResponse.Character(
+                stats: MapleStory.CharacterListResponse.CharacterStats(
+                    id: 1,
+                    name: "Admin",
+                    gender: .male,
+                    skinColor: .normal,
+                    face: 20000,
+                    hair: 30030,
+                    value0: 0,
+                    value1: 0,
+                    value2: 0,
+                    level: 254,
+                    job: .buccaneer,
+                    str: 32767,
+                    dex: 32767,
+                    int: 32767,
+                    luk: 32767,
+                    hp: 30000,
+                    maxHp: 30000,
+                    mp: 26474,
+                    maxMp: 30000,
+                    ap: 0,
+                    sp: 0,
+                    exp: 0,
+                    fame: 13337,
+                    isMarried: 0,
+                    currentMap: 910000000,
+                    spawnPoint: 1,
+                    value3: 0
+                ),
+                appearance: MapleStory.CharacterListResponse.CharacterAppeareance(
+                    gender: .male,
+                    skinColor: .normal,
+                    face: 20000,
+                    mega: true,
+                    hair: 30030,
+                    equipment: [5: 0x82DE0F00, 6: 0xA22C1000, 9: 0xD9D01000, 1: 0x754B0F00, 7: 0x815B1000, 11: 0x279D1600],
+                    maskedEquipment: [:],
+                    cashWeapon: 0,
+                    value0: 0,
+                    value1: 0
+                ),
+                rank: MapleStory.CharacterListResponse.Rank(
+                    worldRank: 1,
+                    rankMove: 0,
+                    jobRank: 1,
+                    jobRankMove: 0
+                )
+            )
+        ])
+        
+        XCTAssertEncode(value, packet)
+        XCTAssertDecode(value, packet)
+        XCTAssertEqual(packet.opcode, type(of: value).opcode)
+        
+        let encrypted = try packet.encrypt(
+            key: .default,
+            nonce: nonce,
+            version: .v62
+        )
+        
+        XCTAssertEqual(encrypted.length, packet.data.count)
+        XCTAssertEqual(encrypted.data, encryptedData)
+    }
+    
+    func testAllCharactersCount() throws {
+        
+        /*
+         MaplePacketEncoder will write encrypted 08 00 01 01 00 00 00 03 00 00 00
+         MaplePacketEncoder header 05 F2 0E F2
+         MaplePacketEncoder custom encrypted 77 B6 48 07 99 7A 0E 5D 97 BE 32
+         MapleAESOFB.crypt() input: 77 B6 48 07 99 7A 0E 5D 97 BE 32
+         MapleAESOFB.crypt() iv: 97 86 C4 0D
+         MapleAESOFB.crypt() output: E2 29 45 34 B6 76 C7 D2 18 E4 CF
+         MaplePacketEncoder AES encrypted E2 29 45 34 B6 76 C7 D2 18 E4 CF
+         MaplePacketEncoder output 05 F2 0E F2 E2 29 45 34 B6 76 C7 D2 18 E4 CF
+         */
+        
+        let packetData = Data([0x08, 0x00, 0x01, 0x01, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00])
+        let encryptedData = Data([0x05, 0xF2, 0x0E, 0xF2, 0xE2, 0x29, 0x45, 0x34, 0xB6, 0x76, 0xC7, 0xD2, 0x18, 0xE4, 0xCF])
+        let nonce: Nonce = 0x9786C40D
+        
+        guard let packet = Packet(data: packetData) else {
+            XCTFail()
+            return
+        }
+        
+        let value = AllCharactersResponse.count(characters: 1, value0: 3)
+        
+        XCTAssertEncode(value, packet)
+        XCTAssertDecode(value, packet)
+        XCTAssertEqual(packet.opcode, type(of: value).opcode)
+        
+        let encrypted = try packet.encrypt(
+            key: .default,
+            nonce: nonce,
+            version: .v62
+        )
+        
+        XCTAssertEqual(encrypted.length, packet.data.count)
+        XCTAssertEqual(encrypted.data, encryptedData)
+    }
 }
