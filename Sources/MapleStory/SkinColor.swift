f167668b7ae360f942a43a7d8a0c13105a19e336
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-21 16:53:24 -0800

    Add `SkinColor`

---
+++ Sources/MapleStory/SkinColor.swift
@@ -0,0 +1,17 @@
+//
+//  SkinColor.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 12/21/22.
+//
+
+/// Skin Color
+public enum SkinColor: UInt8, Codable, CaseIterable {
+    
+    case normal     = 0
+    case dark       = 1
+    case black      = 2
+    case pale       = 3
+    case blue       = 4
+    case white      = 9
+}
