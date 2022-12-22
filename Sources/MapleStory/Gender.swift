9ca78170eda67ee45ffad196372949510122c2de
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-21 16:53:08 -0800

    Add `Gender`

---
+++ Sources/MapleStory/Gender.swift
@@ -0,0 +1,14 @@
+//
+//  Gender.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 12/21/22.
+//
+
+/// Character Gender
+public enum Gender: UInt8, Codable, CaseIterable {
+    
+    case male       = 0
+    
+    case female     = 1
+}
