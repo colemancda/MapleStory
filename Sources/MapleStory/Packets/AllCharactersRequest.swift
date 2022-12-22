380344f54aaef71c669c0ebd95f526c9b4620a3b
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-12-21 22:33:46 -0800

    Add `AllCharactersRequest`

---
+++ Sources/MapleStory/Packets/AllCharactersRequest.swift
@@ -0,0 +1,13 @@
+//
+//  AllCharactersRequest.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 12/21/22.
+//
+
+import Foundation
+
+public struct AllCharactersRequest: MapleStoryPacket, Codable, Equatable, Hashable {
+    
+    public static var opcode: Opcode { 0x0D }
+}
