45e7f0a091c3cabb5b4cbf99b2bc6c88b382cb07
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-04-24 17:48:24 -0400

    Update `MapleStat`

--- Sources/MapleStory/MapleStat.swift
+++ Sources/MapleStory/MapleStat.swift
@@ -15,81 +15,107 @@ public struct MapleStat: OptionSet {
     }
 }
 
-public extension MapleStat {
+// MARK: - Extensions
 
+public extension MapleStat {
+    
+    init?(byte5Encoding value: UInt32) {
+        switch value {
+        case 64:
+            self = .str
+        case 128:
+            self = .dex
+        case 256:
+            self = .int
+        case 512:
+            self = .luk
+        default:
+            return nil
+        }
+    }
+}
+
+// MARK: - Constants
+
+public extension MapleStat {
+    
     static var skin: MapleStat {
         return MapleStat(rawValue: 0x1)
     }
-
+    
     static var face: MapleStat {
         return MapleStat(rawValue: 0x2)
     }
-
+    
     static var hair: MapleStat {
         return MapleStat(rawValue: 0x4)
     }
-
+    
     static var level: MapleStat {
         return MapleStat(rawValue: 0x10)
     }
-
+    
     static var job: MapleStat {
         return MapleStat(rawValue: 0x20)
     }
-
+    
     static var str: MapleStat {
         return MapleStat(rawValue: 0x40)
     }
-
+    
     static var dex: MapleStat {
         return MapleStat(rawValue: 0x80)
     }
-
+    
     static var int: MapleStat {
         return MapleStat(rawValue: 0x100)
     }
-
+    
     static var luk: MapleStat {
         return MapleStat(rawValue: 0x200)
     }
-
+    
     static var hp: MapleStat {
         return MapleStat(rawValue: 0x400)
     }
-
-    static var maxhp: MapleStat {
+    
+    static var maxHP: MapleStat {
         return MapleStat(rawValue: 0x800)
     }
-
+    
     static var mp: MapleStat {
         return MapleStat(rawValue: 0x1000)
     }
-
-    static var maxmp: MapleStat {
+    
+    static var maxMP: MapleStat {
         return MapleStat(rawValue: 0x2000)
     }
-
+    
     static var availableAP: MapleStat {
         return MapleStat(rawValue: 0x4000)
     }
-
+    
     static var availableSP: MapleStat {
         return MapleStat(rawValue: 0x8000)
     }
-
+    
     static var exp: MapleStat {
         return MapleStat(rawValue: 0x10000)
     }
-
+    
     static var fame: MapleStat {
         return MapleStat(rawValue: 0x20000)
     }
-
+    
     static var meso: MapleStat {
         return MapleStat(rawValue: 0x40000)
     }
-
+    
     static var pet: MapleStat {
         return MapleStat(rawValue: 0x180008)
+    }
+    
+    static var gachaExp: MapleStat {
+        return MapleStat(rawValue: 0x200000)
     }
 }
