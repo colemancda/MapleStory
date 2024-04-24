bb3240f2fc3bee6c3bb5dd80a99879bf0c2cb9a7
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-04-24 17:22:40 -0400

    Update `SkinColor` for v83

--- Sources/MapleStory/SkinColor.swift
+++ Sources/MapleStory/SkinColor.swift
@@ -13,5 +13,7 @@ public enum SkinColor: UInt8, Codable, CaseIterable {
     case black      = 2
     case pale       = 3
     case blue       = 4
+    case green      = 5
     case white      = 9
+    case pink       = 10
 }
