66253109d9f4bbc4c1bf3dc6bf76ca5b38b71b81
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-04-25 05:21:49 -0400

    Add `Codable` conformance to `Version`

--- Sources/MapleStory/Enum/Version.swift
+++ Sources/MapleStory/Enum/Version.swift
@@ -6,7 +6,7 @@
 //
 
 /// MapleStory Version
-public struct Version: RawRepresentable, Equatable, Hashable, Codable {
+public struct Version: RawRepresentable, Equatable, Hashable, Codable, Sendable {
     
     public let rawValue: UInt16
     
@@ -37,22 +37,47 @@ extension Version: CustomStringConvertible, CustomDebugStringConvertible {
     }
 }
 
+// MARK: - Comparable
+
+extension Version: Comparable {
+    
+    public static func < (lhs: Version, rhs: Version) -> Bool {
+        lhs.rawValue < rhs.rawValue
+    }
+    
+    public static func > (lhs: Version, rhs: Version) -> Bool {
+        lhs.rawValue > rhs.rawValue
+    }
+}
+
 // MARK: - Version
 
 public extension Version {
     
     /// MapleStory v28
-    static var v28: Version { 28 }
+    static var v28: Version { 28 } // Pianus boss added
     
     /// MapleStory v40
-    static var v40: Version { 40 }
+    static var v40: Version { 40 } // Leaf City added
+    
+    /// MapleStory v49
+    static var v49: Version { 49 } // 4th Job advancement added
     
     /// MapleStory v55
-    static var v55: Version { 55 }
+    static var v55: Version { 55 } // Dragon Mount added
     
     /// MapleStory v62
-    static var v62: Version { 62 }
+    static var v62: Version { 62 } // Pirate Class added
+    
+    /// MapleStory v73
+    static var v73: Version { 73 } // Cygnus Knights added
+    
+    /// MapleStory v80
+    static var v80: Version { 80 } // Aran class added
     
     /// MapleStory v83
-    static var v83: Version { 83 }
+    static var v83: Version { 83 } // Neo City added
+    
+    /// MapleStory v93
+    static var v93: Version { 93 } // Big Bang
 }
