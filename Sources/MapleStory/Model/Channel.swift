9fcce728983493d777e3f39f1088629b99b1b0f9
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-04-25 05:32:46 -0400

    Add `Entity` conformance to `Channel`

--- Sources/MapleStory/Model/Channel.swift
+++ Sources/MapleStory/Model/Channel.swift
@@ -26,7 +26,7 @@ public struct Channel: Codable, Equatable, Hashable, Identifiable, Sendable {
     public var status: Status
     
     public init(
-        id: UUID,
+        id: UUID = UUID(),
         index: Index,
         world: World.ID,
         name: String,
@@ -39,5 +39,42 @@ public struct Channel: Codable, Equatable, Hashable, Identifiable, Sendable {
         self.load = load
         self.status = status
         self.world = world
+    }
+    
+    public enum CodingKeys: CodingKey {
+        
+        case id
+        case index
+        case world
+        case name
+        case load
+        case status
+    }
+}
+
+// MARK: - Entity
+
+extension Channel: Entity {
+    
+    public static var attributes: [CodingKeys: AttributeType] {
+        [
+            .name: .string,
+            .index: .int16,
+            .name: .string,
+            .load: .int64,
+            .status: .int16
+        ]
+    }
+    
+    public static var relationships: [CodingKeys: Relationship] {
+        [
+            .world: Relationship(
+                id: .world,
+                entity: Channel.self,
+                destination: World.self,
+                type: .toOne,
+                inverseRelationship: .channels
+            )
+        ]
     }
 }
