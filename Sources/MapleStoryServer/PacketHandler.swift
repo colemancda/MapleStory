46cd691bf115989b8250e186df21692e1cdef761
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-04-26 18:51:49 -0400

    Add `ServerHandler`

--- Sources/MapleStoryServer/PacketHandler.swift
+++ Sources/MapleStoryServer/PacketHandler.swift
@@ -6,12 +6,34 @@
 //
 
 import Foundation
+import MapleStory
+import CoreModel
 
-public protocol PacketHandler {
+public protocol ServerHandler {
     
-    associatedtype Event
+    associatedtype Socket: MapleStorySocket
     
-    init(event: Event)
+    associatedtype Storage: ModelStorage
     
-    func evaluate() async throws
+    var connection: MapleStoryServer<Socket, Storage>.Connection { get }
+    
+    init(connection: MapleStoryServer<Socket, Storage>.Connection)
+    
+    func didConnect()
+    
+    func didDisconnect()
+}
+
+public extension ServerHandler {
+    
+    func didConnect() { }
+    
+    func didDisconnect() { }
+}
+
+public protocol PacketHandler: ServerHandler {
+    
+    associatedtype Packet: MapleStoryPacket
+    
+    func handle(packet: Packet) async throws
 }
