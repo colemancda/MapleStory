9f2b9e350ab7348bc7b67c6f1dcf69ec3aa551c6
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-04-29 08:46:11 -0400

    Add `PinCodeStatus`

---
+++ Sources/MapleStory/Enum/PinCodeStatus.swift
@@ -0,0 +1,27 @@
+//
+//  PinCodeStatus.swift
+//
+//
+//  Created by Alsey Coleman Miller on 4/29/24.
+//
+
+import Foundation
+
+/// Pin Code Status
+public enum PinCodeStatus: UInt8, Codable, CaseIterable, Sendable {
+    
+    /// PIN was accepted.
+    case success            = 0
+    
+    /// Register a new PIN
+    case register           = 1
+    
+    /// Invalid pin / Reenter
+    case invalid            = 2
+    
+    /// Connection failed due to system error
+    case systemError        = 3
+    
+    /// Enter the pin
+    case enterPin           = 4
+}
