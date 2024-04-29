f0c36edc7544f152bc9feb02e5aea69c263c1093
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-04-29 09:50:24 -0400

    Fixed v83 `LoginRequest`

--- Sources/MapleStory83/Packets/LoginRequest.swift
+++ Sources/MapleStory83/Packets/LoginRequest.swift
@@ -9,11 +9,9 @@ import Foundation
 
 /// Login request
 public struct LoginRequest: MapleStoryPacket, Decodable, Equatable, Hashable {
-    
-    public static var clientOpcode: ClientOpcode { .loginPassword }
-    
-    public static var opcode: Opcode { 0x01 }
-    
+        
+    public static var opcode: Opcode { .init(client: .loginRequest) }
+
     public var username: String
     
     public var password: String
