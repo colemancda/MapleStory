cc1f127b3b2d671e0199311714fa97e57e5c0e74
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-04-30 23:20:28 -0400

    Add `MapleStoryServer.Connection.createCharacter()`

---
+++ Sources/MapleStoryServer/Handlers/CreateCharacterHandler.swift
@@ -0,0 +1,41 @@
+//
+//  CreateCharacterHandler.swift
+//
+//
+//  Created by Alsey Coleman Miller on 4/30/24.
+//
+
+import Foundation
+import MapleStory
+import CoreModel
+
+public extension MapleStoryServer.Connection {
+    
+    func createCharacter(
+        _ values: Character.CreationValues
+    ) async throws -> Character {
+        log("Create character - \(values.name)")
+        guard var user = try await self.user else {
+            throw MapleStoryError.notAuthenticated
+        }
+        guard var world = try await self.world else {
+            throw MapleStoryError.invalidRequest
+        }
+        let newCharacterID = world.newCharacter()
+        let newCharacter = Character(
+            create: values,
+            index: newCharacterID,
+            user: user.id,
+            world: world.id
+        )
+        user.characters.append(newCharacter.id)
+        world.characters.append(newCharacter.id)
+        let modelData = try [
+            newCharacter.encode(),
+            user.encode(),
+            world.encode()
+        ]
+        try await database.insert(modelData)
+        return newCharacter
+    }
+}
