b23e315de695707c937a5cebdd40275c89f19d66
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-04-30 21:22:12 -0400

    Add `World.version`

--- Sources/MapleStory/Enum/WorldName.swift
+++ Sources/MapleStory/Enum/WorldName.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public extension World {
     
-    enum Name: String, Codable, CaseIterable {
+    enum Name: String, Codable, CaseIterable, Sendable {
         
         case scania     = "Scania"
         case bera       = "Bera"
@@ -21,10 +21,18 @@ public extension World {
         case kradia     = "Kradia"
         case yellonde   = "Yellonde"
         case demethos   = "Demethos"
+        case galicia    = "Galicia"
+        
+        // Big-Bang
         case elnido     = "Elnido"
+        case zenith     = "Zenith"
+        
+        // Post Big Bang
+        case arcania    = "Arcania"
+        case chaos      = "Chaos"
+        case nova       = "Nova"
         case kastia     = "Kastia"
         case judis      = "Judis"
-        case arkenia    = "Arkenia"
         case plana      = "Plana"
     }
 }
@@ -40,5 +48,43 @@ public extension World.Name {
     
     var index: World.Index {
         World.Index(Self.allCases.firstIndex(of: self)!)
+    }
+    
+    /// Version the world was introduced.
+    var version: Version {
+        switch self {
+        case .scania:
+            return 1
+        case .bera:
+            return 2
+        case .broa:
+            return 7
+        case .windia:
+            return .v14
+        case .khaini:
+            return 18
+        case .bellocan:
+            return .v28
+        case .mardia:
+            return 35
+        case .kradia:
+            return 44
+        case .yellonde:
+            return 59
+        case .demethos:
+            return .v62
+        case .galicia:
+            return .v80
+        case .elnido:
+            return .v93
+        case .zenith:
+            return .v93
+        default:
+            return 94 // Post-Big Bang
+        }
+    }
+    
+    func isCompatible(for version: MapleStory.Version) -> Bool {
+        version >= self.version
     }
 }
