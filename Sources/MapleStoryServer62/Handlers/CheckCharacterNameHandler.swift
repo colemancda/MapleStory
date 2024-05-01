a3b678d2346bc54d808a1b98ddb9aa86fbfd8c37
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-05-01 06:19:18 -0400

    Add v62 `CheckCharacterNameHandler`

---
+++ Sources/MapleStoryServer62/Handlers/CheckCharacterNameHandler.swift
@@ -0,0 +1,37 @@
+//
+//  CheckCharacterNameHandler.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 4/30/24.
+//
+
+import Foundation
+import CoreModel
+import MapleStory62
+import MapleStoryServer
+
+public struct CheckCharacterNameHandler: PacketHandler {
+    
+    public typealias Packet = MapleStory62.CheckNameRequest
+        
+    public init() { }
+    
+    public func handle<Socket: MapleStorySocket, Database: ModelStorage>(
+        packet: Packet,
+        connection: MapleStoryServer<Socket, Database>.Connection
+    ) async throws {
+        let response = try await checkCharacterName(packet, connection: connection)
+        try await connection.respond(response)
+    }
+}
+
+internal extension CheckCharacterNameHandler {
+    
+    func checkCharacterName<Socket: MapleStorySocket, Database: ModelStorage>(
+        _ request: MapleStory62.CheckNameRequest,
+        connection: MapleStoryServer<Socket, Database>.Connection
+    ) async throws -> MapleStory62.CheckNameResponse {
+        let isAvailable = try await connection.checkCharacterName(request.name)
+        return .init(name: request.name, isUsed: !isAvailable)
+    }
+}
