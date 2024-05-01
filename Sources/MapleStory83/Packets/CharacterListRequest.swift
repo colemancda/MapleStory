491c7eec818e8219d54b3324aceb78fae37bb1a8
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-05-01 03:37:37 -0400

    Fixed v83 `CharacterListRequest`

--- Sources/MapleStory83/Packets/CharacterListRequest.swift
+++ Sources/MapleStory83/Packets/CharacterListRequest.swift
@@ -7,19 +7,32 @@
 
 import Foundation
 
-public struct CharacterListRequest: MapleStoryPacket, Decodable, Equatable, Hashable {
+public struct CharacterListRequest: MapleStoryPacket, Codable, Equatable, Hashable, Sendable {
     
     public static var opcode: Opcode { .init(client: .characterListRequest) }
     
-    internal var value0: UInt8
+    internal let value0: UInt8
     
     public let world: World.Index
     
     public let channel: Channel.Index
     
-    public init(world: World.Index, channel: Channel.Index) {
+    internal let value1: UInt32
+    
+    public init(
+        world: World.Index,
+        channel: Channel.Index
+    ) {
         self.world = world
         self.channel = channel
         self.value0 = 0x00
+        self.value1 = 0x00
+    }
+    
+    internal init(value0: UInt8, world: World.Index, channel: Channel.Index, value1: UInt32) {
+        self.value0 = value0
+        self.world = world
+        self.channel = channel
+        self.value1 = value1
     }
 }
