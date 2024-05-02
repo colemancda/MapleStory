6c1b927307e78a088814e941b3219b912364712a
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-05-01 21:48:12 -0400

    Add v28 `HandshakeHandler`

---
+++ Sources/MapleStoryServer28/Handlers/HandshakeHandler.swift
@@ -0,0 +1,42 @@
+//
+//  HandshakeHandler.swift
+//
+//
+//  Created by Alsey Coleman Miller on 5/1/24.
+//
+
+import Foundation
+import MapleStory28
+import MapleStoryServer
+import CoreModel
+
+public struct HandshakeHandler: ServerHandler {
+    
+    public func didConnect<Socket, Storage>(
+        connection: MapleStoryServer<Socket, Storage>.Connection
+    ) where Socket : MapleStorySocket, Storage : ModelStorage {
+        Task {
+            do {
+                try await sendHandshake(connection: connection)
+            }
+            catch {
+                await connection.close(error)
+            }
+        }
+    }
+}
+
+internal extension HandshakeHandler {
+    
+    func sendHandshake<Socket: MapleStorySocket, Storage: ModelStorage>(
+        connection: MapleStoryServer<Socket, Storage>.Connection
+    )  async throws {
+        let packet = await MapleStory28.HelloPacket(
+            recieveNonce: await connection.recieveNonce,
+            sendNonce: await connection.sendNonce,
+            region: connection.region
+        )
+        try await connection.send(packet)
+        await connection.encrypt()
+    }
+}
