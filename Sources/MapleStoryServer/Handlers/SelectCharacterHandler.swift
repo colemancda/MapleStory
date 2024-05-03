2496cbc1fd915d81070de65d3e738b559d6613e4
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-05-03 09:14:35 -0400

    Add `MapleStoryServer.Connection.selectCharacter()`

--- Sources/MapleStoryServer/Handlers/SelectCharacterHandler.swift
+++ Sources/MapleStoryServer/Handlers/SelectCharacterHandler.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import MapleStory
+
+public extension MapleStoryServer.Connection {
+    
+    /// Select Character
+    func selectCharacter(
+        _ characterIndex: Character.Index
+    ) async throws -> MapleStoryAddress {
+        log("Select Character \(characterIndex)")
+        guard let user = try await self.user else {
+            throw MapleStoryError.notAuthenticated
+        }
+        guard let world = try await self.world else {
+            throw MapleStoryError.invalidWorld
+        }
+        guard let channel = try await self.channel else {
+            throw MapleStoryError.invalidChannel
+        }
+        // TODO: Add session in DB
+        return world.address
+    }
+}
