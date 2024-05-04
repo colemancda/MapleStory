1bb31e7bfd9f9274f1a84f5a5ea157173285b225
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-05-04 08:48:35 -0400

    Add v62 `ServerListRerequestHandler`

---
+++ Sources/MapleStoryServer62/Handlers/ServerListRerequestHandler.swift
@@ -0,0 +1,46 @@
+//
+//  ServerListRerequestHandler.swift
+//
+//
+//  Created by Alsey Coleman Miller on 5/4/24.
+//
+
+import Foundation
+import CoreModel
+import MapleStory62
+import MapleStoryServer
+
+/// MapleStory v62 World List Server handler
+public struct ServerListRerequestHandler: PacketHandler {
+    
+    public typealias Packet = MapleStory62.ServerListRerequest
+    
+    public init() { }
+    
+    public func handle<Socket: MapleStorySocket, Database: ModelStorage>(
+        packet: Packet,
+        connection: MapleStoryServer<Socket, Database, ClientOpcode, ServerOpcode>.Connection
+    ) async throws {
+        do {
+            // world list
+            let responses = try await worldList(packet, connection: connection)
+            for response in responses {
+                try await connection.send(response)
+            }
+        }
+        catch {
+            await connection.close(error)
+        }
+    }
+}
+
+internal extension ServerListRerequestHandler {
+    
+    func worldList<Socket: MapleStorySocket, Database: ModelStorage>(
+        _ request: MapleStory62.ServerListRerequest,
+        connection: MapleStoryServer<Socket, Database, ClientOpcode, ServerOpcode>.Connection
+    ) async throws -> [MapleStory62.ServerListResponse] {
+        try await connection.listWorlds()
+            .map { .world(.init(world: $0.world, channels: $0.channels)) } + [.end]
+    }
+}
