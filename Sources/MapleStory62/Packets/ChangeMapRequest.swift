10ce19b52bd8dd14df2a7565641859165ce79c80
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 00:40:25 -0400

    Add `ChangeMapRequest`

---
+++ Sources/MapleStory62/Packets/ChangeMapRequest.swift
@@ -0,0 +1,26 @@
+//
+//  ChangeMapRequest.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/24/26.
+//
+
+import Foundation
+
+public struct ChangeMapRequest: MapleStoryPacket, Codable, Equatable, Hashable, Sendable {
+
+    public static var opcode: ClientOpcode { .changeMap }
+
+    /// 1 = death return, 2 = regular portal
+    public let type: UInt8
+
+    /// Target map ID (0x3B9AC9FF = none / use portal)
+    public let targetMap: UInt32
+
+    /// Portal name to warp through
+    public let portalName: String
+
+    internal let value0: UInt16
+
+    internal let value1: UInt16
+}
