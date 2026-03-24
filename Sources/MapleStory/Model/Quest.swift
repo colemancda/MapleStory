7659835f57ec6403d40ac26117fa1d4e4ebffc2e
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 14:02:30 -0400

    Add `QuestStatus`

---
+++ Sources/MapleStory/Model/Quest.swift
@@ -0,0 +1,141 @@
+//
+//  Quest.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/24/26.
+//
+
+import Foundation
+import CoreModel
+import MapleStory
+
+/// Quest ID
+public typealias QuestID = UInt16
+
+/// Quest status
+public enum QuestStatus: UInt8, Codable, Equatable, Hashable, Sendable {
+    case notStarted = 0
+    case started = 1
+    case completed = 2
+}
+
+/// Quest completion state for a character
+public struct QuestState: Codable, Equatable, Hashable, Sendable {
+
+    /// Quest ID
+    public let questID: QuestID
+
+    /// Current status
+    public var status: QuestStatus
+
+    /// Time when quest was started
+    public var startTime: Date?
+
+    /// Time when quest was completed
+    public var completionTime: Date?
+
+    /// Number of times completed (for repeatable quests)
+    public var completionCount: UInt8
+
+    /// Quest-specific data (mob kills, item collection, etc.)
+    public var progress: [String: UInt32] = [:]
+
+    /// Create quest state
+    public init(
+        questID: QuestID,
+        status: QuestStatus = .notStarted,
+        startTime: Date? = nil,
+        completionTime: Date? = nil,
+        completionCount: UInt8 = 0
+    ) {
+        self.questID = questID
+        self.status = status
+        self.startTime = startTime
+        self.completionTime = completionTime
+        self.completionCount = completionCount
+    }
+}
+
+/// Quest reward
+public struct QuestReward: Codable, Equatable, Hashable, Sendable {
+
+    /// EXP reward
+    public let exp: UInt32
+
+    /// Meso reward
+    public let meso: UInt32
+
+    /// Item rewards (item ID -> quantity)
+    public let items: [UInt32: UInt16]
+
+    /// Job-specific rewards
+    public let job: UInt16?
+
+    /// Create quest reward
+    public init(
+        exp: UInt32 = 0,
+        meso: UInt32 = 0,
+        items: [UInt32: UInt16] = [:],
+        job: UInt16? = nil
+    ) {
+        self.exp = exp
+        self.meso = meso
+        self.items = items
+        self.job = job
+    }
+}
+
+/// Quest requirement
+public struct QuestRequirement: Codable, Equatable, Hashable, Sendable {
+
+    /// Minimum level
+    public let minLevel: UInt8
+
+    /// Maximum level
+    public let maxLevel: UInt8
+
+    /// Required job (0 = any)
+    public let job: UInt16?
+
+    /// Required quests (quest ID -> completion count)
+    public let completedQuests: [QuestID: UInt8]
+
+    /// Create quest requirement
+    public init(
+        minLevel: UInt8 = 0,
+        maxLevel: UInt8 = 200,
+        job: UInt16? = nil,
+        completedQuests: [QuestID: UInt8] = [:]
+    ) {
+        self.minLevel = minLevel
+        self.maxLevel = maxLevel
+        self.job = job
+        self.completedQuests = completedQuests
+    }
+
+    /// Check if character meets requirements
+    public func canStart(character: Character, questStates: [QuestID: QuestState]) -> Bool {
+        // Check level
+        guard character.level >= UInt16(minLevel) && character.level <= UInt16(maxLevel) else {
+            return false
+        }
+
+        // Check job
+        if let requiredJob = job {
+            // TODO: Check job compatibility
+        }
+
+        // Check completed quests
+        for (questID, requiredCount) in completedQuests {
+            if let state = questStates[questID] {
+                if state.completionCount < requiredCount {
+                    return false
+                }
+            } else {
+                return false
+            }
+        }
+
+        return true
+    }
+}
