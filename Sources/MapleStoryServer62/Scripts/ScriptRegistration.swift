0acc25cd738afb09ff659209bbc97726c66cd08b
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 06:45:05 -0400

    Add `NPCScriptRegistry.registerAll()`

---
+++ Sources/MapleStoryServer62/Scripts/ScriptRegistration.swift
@@ -0,0 +1,22 @@
+//
+//  ScriptRegistration.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/24/26.
+//
+
+import Foundation
+import MapleStory62
+
+public extension NPCScriptRegistry {
+
+    /// Register all NPC scripts.
+    func registerAll() {
+        registerLithHarbor()
+        registerHenesys()
+        registerEllinia()
+        registerPerion()
+        registerKerningCity()
+        registerEventNPCs()
+    }
+}
