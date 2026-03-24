d6a42910de74ec5bbd7f569578fae6a9bbe9a957
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 11:33:27 -0400

    Fixed `ClientStartError` decoding

--- Sources/MapleStory62/Packets/ClientStartError.swift
+++ Sources/MapleStory62/Packets/ClientStartError.swift
@@ -19,6 +19,24 @@ public struct ClientStartError: MapleStoryPacket, Equatable, Hashable, Codable, 
     }
 }
 
+// MARK: - MapleStoryDecodable
+
+extension ClientStartError: MapleStoryDecodable {
+    
+    public init(from container: MapleStoryDecodingContainer) throws {
+        let length = Int(try container.decode(UInt16.self))
+        let data = try container.decode(Data.self, length: length)
+        if let string = String(data: data, encoding: .ascii) ?? String(data: data, encoding: .isoLatin1) {
+            self.error = string
+        } else {
+            throw DecodingError.dataCorrupted(DecodingError.Context(
+                codingPath: container.codingPath,
+                debugDescription: "Invalid error string"
+            ))
+        }
+    }
+}
+
 // MARK: - ExpressibleByStringLiteral
 
 extension ClientStartError: ExpressibleByStringLiteral {
--- Sources/MapleStory83/Packets/ClientStartError.swift
+++ Sources/MapleStory83/Packets/ClientStartError.swift
@@ -18,3 +18,30 @@ public struct ClientStartError: MapleStoryPacket, Equatable, Hashable, Codable, 
         self.error = error
     }
 }
+
+// MARK: - MapleStoryDecodable
+
+extension ClientStartError: MapleStoryDecodable {
+    
+    public init(from container: MapleStoryDecodingContainer) throws {
+        let length = Int(try container.decode(UInt16.self))
+        let data = try container.decode(Data.self, length: length)
+        if let string = String(data: data, encoding: .ascii) ?? String(data: data, encoding: .isoLatin1) {
+            self.error = string
+        } else {
+            throw DecodingError.dataCorrupted(DecodingError.Context(
+                codingPath: container.codingPath,
+                debugDescription: "Invalid error string"
+            ))
+        }
+    }
+}
+
+// MARK: - ExpressibleByStringLiteral
+
+extension ClientStartError: ExpressibleByStringLiteral {
+    
+    public init(stringLiteral value: StringLiteralType) {
+        self.error = value
+    }
+}
