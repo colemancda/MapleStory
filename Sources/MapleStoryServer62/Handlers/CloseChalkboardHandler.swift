f974a2515e1853ffff5309ff13a82a808836ef1b
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 06:16:33 -0400

    Add `CloseChalkboardHandler`

---
+++ Sources/MapleStoryServer62/Handlers/CloseChalkboardHandler.swift
@@ -0,0 +1,25 @@
+//
+//  CloseChalkboardHandler.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/24/26.
+//
+
+import Foundation
+import CoreModel
+import MapleStory62
+import MapleStoryServer
+
+public struct CloseChalkboardHandler: PacketHandler {
+
+    public typealias Packet = MapleStory62.CloseChalkboardRequest
+
+    public init() { }
+
+    public func handle<Socket: MapleStorySocket, Database: ModelStorage>(
+        packet: Packet,
+        connection: MapleStoryServer<Socket, Database, ClientOpcode, ServerOpcode>.Connection
+    ) async throws {
+        // Close chalkboard message — not yet implemented.
+    }
+}
