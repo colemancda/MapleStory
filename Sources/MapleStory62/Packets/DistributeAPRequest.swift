1f0c74c9e1db7e3f72f6844873f99d713d7eb1e4
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 00:40:32 -0400

    Add `DistributeAPRequest`

---
+++ Sources/MapleStory62/Packets/DistributeAPRequest.swift
@@ -0,0 +1,20 @@
+//
+//  DistributeAPRequest.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/24/26.
+//
+
+import Foundation
+
+/// Client request to spend an AP point on a stat.
+/// Stat values: STR=64, DEX=128, INT=256, LUK=512, HP=2048, MP=8192
+public struct DistributeAPRequest: MapleStoryPacket, Codable, Equatable, Hashable, Sendable {
+
+    public static var opcode: ClientOpcode { .distributeAP }
+
+    internal let value0: UInt32
+
+    /// Stat to increase (maps to `MapleStat` raw value)
+    public let stat: UInt32
+}
