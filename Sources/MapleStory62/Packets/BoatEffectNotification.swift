dddb97bc726f8bf78f0c4ad36ffd9eda5c03aacc
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 17:23:05 -0400

    Add `BoatEffectNotification`

---
+++ Sources/MapleStory62/Packets/BoatEffectNotification.swift
@@ -0,0 +1,26 @@
+//
+//  BoatEffectNotification.swift
+//
+//
+
+import Foundation
+
+public struct BoatEffectNotification: MapleStoryPacket, Equatable, Hashable, Sendable {
+
+    public static var opcode: ServerOpcode { .boatEffect }
+
+    /// Java sends 1 or 2.
+    public let effect: UInt16
+
+    public init(effect: UInt16) {
+        self.effect = effect
+    }
+}
+
+extension BoatEffectNotification: MapleStoryEncodable {
+
+    public func encode(to container: MapleStoryEncodingContainer) throws {
+        try container.encode(effect, isLittleEndian: true)
+    }
+}
+
