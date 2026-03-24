5a51b4ec6181f60908d4507943cf4474d4d59c24
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 06:16:32 -0400

    Add `AutoAggroHandler`

---
+++ Sources/MapleStoryServer62/Handlers/AutoAggroHandler.swift
@@ -0,0 +1,25 @@
+//
+//  AutoAggroHandler.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/24/26.
+//
+
+import Foundation
+import CoreModel
+import MapleStory62
+import MapleStoryServer
+
+public struct AutoAggroHandler: PacketHandler {
+
+    public typealias Packet = MapleStory62.AutoAggroRequest
+
+    public init() { }
+
+    public func handle<Socket: MapleStorySocket, Database: ModelStorage>(
+        packet: Packet,
+        connection: MapleStoryServer<Socket, Database, ClientOpcode, ServerOpcode>.Connection
+    ) async throws {
+        // Monster aggro trigger — not yet implemented.
+    }
+}
