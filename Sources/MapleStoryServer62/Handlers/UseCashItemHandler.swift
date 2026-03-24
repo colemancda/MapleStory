7e22df8c57152027839e6f86fb5146068496e303
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 06:16:35 -0400

    Add `UseCashItemHandler`

---
+++ Sources/MapleStoryServer62/Handlers/UseCashItemHandler.swift
@@ -0,0 +1,25 @@
+//
+//  UseCashItemHandler.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/24/26.
+//
+
+import Foundation
+import CoreModel
+import MapleStory62
+import MapleStoryServer
+
+public struct UseCashItemHandler: PacketHandler {
+
+    public typealias Packet = MapleStory62.UseCashItemRequest
+
+    public init() { }
+
+    public func handle<Socket: MapleStorySocket, Database: ModelStorage>(
+        packet: Packet,
+        connection: MapleStoryServer<Socket, Database, ClientOpcode, ServerOpcode>.Connection
+    ) async throws {
+        // Use cash shop item — not yet implemented.
+    }
+}
