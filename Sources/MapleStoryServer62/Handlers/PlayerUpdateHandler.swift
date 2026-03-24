0b2102569f245d324cea373b2d14e4ff8cbecf50
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 00:34:54 -0400

    Add `PlayerUpdateHandler`

---
+++ Sources/MapleStoryServer62/Handlers/PlayerUpdateHandler.swift
@@ -0,0 +1,25 @@
+//
+//  PlayerUpdateHandler.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/24/26.
+//
+
+import Foundation
+import CoreModel
+import MapleStory62
+import MapleStoryServer
+
+public struct PlayerUpdateHandler: PacketHandler {
+
+    public typealias Packet = MapleStory62.PlayerUpdateRequest
+
+    public init() { }
+
+    public func handle<Socket: MapleStorySocket, Database: ModelStorage>(
+        packet: Packet,
+        connection: MapleStoryServer<Socket, Database, ClientOpcode, ServerOpcode>.Connection
+    ) async throws {
+        // Client stat refresh request — no response required.
+    }
+}
