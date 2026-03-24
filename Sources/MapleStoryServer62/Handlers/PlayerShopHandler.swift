f7ebae499e0e7a75e03508ff4384fdf15affde23
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 06:16:34 -0400

    Add `PlayerShopHandler`

---
+++ Sources/MapleStoryServer62/Handlers/PlayerShopHandler.swift
@@ -0,0 +1,25 @@
+//
+//  PlayerShopHandler.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/24/26.
+//
+
+import Foundation
+import CoreModel
+import MapleStory62
+import MapleStoryServer
+
+public struct PlayerShopHandler: PacketHandler {
+
+    public typealias Packet = MapleStory62.PlayerShopRequest
+
+    public init() { }
+
+    public func handle<Socket: MapleStorySocket, Database: ModelStorage>(
+        packet: Packet,
+        connection: MapleStoryServer<Socket, Database, ClientOpcode, ServerOpcode>.Connection
+    ) async throws {
+        // Player shop / mini-game interaction — not yet implemented.
+    }
+}
