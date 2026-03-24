6bdfd9398b5fb09a7d9e13b49836321f7f54d520
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 06:16:35 -0400

    Add `SummonAttackHandler`

---
+++ Sources/MapleStoryServer62/Handlers/SummonAttackHandler.swift
@@ -0,0 +1,25 @@
+//
+//  SummonAttackHandler.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/24/26.
+//
+
+import Foundation
+import CoreModel
+import MapleStory62
+import MapleStoryServer
+
+public struct SummonAttackHandler: PacketHandler {
+
+    public typealias Packet = MapleStory62.SummonAttackRequest
+
+    public init() { }
+
+    public func handle<Socket: MapleStorySocket, Database: ModelStorage>(
+        packet: Packet,
+        connection: MapleStoryServer<Socket, Database, ClientOpcode, ServerOpcode>.Connection
+    ) async throws {
+        // Summon attack — damage calculation not yet implemented.
+    }
+}
