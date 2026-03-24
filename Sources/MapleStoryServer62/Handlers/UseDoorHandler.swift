8571e14c70fcaa365ccc81db990d37eea736880c
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 06:16:35 -0400

    Add `UseDoorHandler`

---
+++ Sources/MapleStoryServer62/Handlers/UseDoorHandler.swift
@@ -0,0 +1,25 @@
+//
+//  UseDoorHandler.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/24/26.
+//
+
+import Foundation
+import CoreModel
+import MapleStory62
+import MapleStoryServer
+
+public struct UseDoorHandler: PacketHandler {
+
+    public typealias Packet = MapleStory62.UseDoorRequest
+
+    public init() { }
+
+    public func handle<Socket: MapleStorySocket, Database: ModelStorage>(
+        packet: Packet,
+        connection: MapleStoryServer<Socket, Database, ClientOpcode, ServerOpcode>.Connection
+    ) async throws {
+        // Use mystic door — not yet implemented.
+    }
+}
