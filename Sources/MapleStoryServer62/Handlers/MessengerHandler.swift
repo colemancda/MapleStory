94ab66d611ede8dcf36d4914638b7b53880e9259
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 06:16:33 -0400

    Add `MessengerHandler`

---
+++ Sources/MapleStoryServer62/Handlers/MessengerHandler.swift
@@ -0,0 +1,25 @@
+//
+//  MessengerHandler.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/24/26.
+//
+
+import Foundation
+import CoreModel
+import MapleStory62
+import MapleStoryServer
+
+public struct MessengerHandler: PacketHandler {
+
+    public typealias Packet = MapleStory62.MessengerRequest
+
+    public init() { }
+
+    public func handle<Socket: MapleStorySocket, Database: ModelStorage>(
+        packet: Packet,
+        connection: MapleStoryServer<Socket, Database, ClientOpcode, ServerOpcode>.Connection
+    ) async throws {
+        // Maple Messenger — not yet implemented.
+    }
+}
