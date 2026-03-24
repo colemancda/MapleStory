6016f54b59de20aee534486cd23c9d86b3b7c6f4
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 13:58:25 -0400

    Add `Storage`

---
+++ Sources/MapleStory/Model/Storage.swift
@@ -0,0 +1,49 @@
+//
+//  Storage.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/24/26.
+//
+
+import Foundation
+import CoreModel
+import MapleStory
+
+/// Character storage inventory
+public struct Storage: Codable, Equatable, Hashable, Sendable {
+
+    /// User ID that owns this storage
+    public let userID: User.ID
+
+    /// Mesos stored
+    public var mesos: UInt32
+
+    /// Number of slots used
+    public var slots: UInt8 {
+        UInt8(items.count)
+    }
+
+    /// Maximum slots (can be expanded)
+    public var maxSlots: UInt8
+
+    /// Items in storage (slot -> item)
+    public var items: [Int8: InventoryItem]
+
+    /// Create storage
+    public init(
+        userID: User.ID,
+        mesos: UInt32 = 0,
+        maxSlots: UInt8 = 4,
+        items: [Int8: InventoryItem] = [:]
+    ) {
+        self.userID = userID
+        self.mesos = mesos
+        self.maxSlots = maxSlots
+        self.items = items
+    }
+
+    /// Check if storage has space for more items
+    public var hasSpace: Bool {
+        return items.count < Int(maxSlots)
+    }
+}
