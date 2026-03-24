0ba6954b06a49974f5fa8e619d5d881c1785d2a8
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 00:40:25 -0400

    Add `ChangeMapHandler`

---
+++ Sources/MapleStoryServer62/Handlers/ChangeMapHandler.swift
@@ -0,0 +1,25 @@
+//
+//  ChangeMapHandler.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/24/26.
+//
+
+import Foundation
+import CoreModel
+import MapleStory62
+import MapleStoryServer
+
+public struct ChangeMapHandler: PacketHandler {
+
+    public typealias Packet = MapleStory62.ChangeMapRequest
+
+    public init() { }
+
+    public func handle<Socket: MapleStorySocket, Database: ModelStorage>(
+        packet: Packet,
+        connection: MapleStoryServer<Socket, Database, ClientOpcode, ServerOpcode>.Connection
+    ) async throws {
+        // Portal / death warp — map transfer not yet implemented.
+    }
+}
