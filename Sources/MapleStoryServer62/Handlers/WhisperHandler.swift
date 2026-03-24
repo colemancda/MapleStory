1940c45b7cebe6e867855fd53a023ed2af57e536
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 06:16:35 -0400

    Add `WhisperHandler`

---
+++ Sources/MapleStoryServer62/Handlers/WhisperHandler.swift
@@ -0,0 +1,25 @@
+//
+//  WhisperHandler.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/24/26.
+//
+
+import Foundation
+import CoreModel
+import MapleStory62
+import MapleStoryServer
+
+public struct WhisperHandler: PacketHandler {
+
+    public typealias Packet = MapleStory62.WhisperRequest
+
+    public init() { }
+
+    public func handle<Socket: MapleStorySocket, Database: ModelStorage>(
+        packet: Packet,
+        connection: MapleStoryServer<Socket, Database, ClientOpcode, ServerOpcode>.Connection
+    ) async throws {
+        // Whisper / find player — not yet implemented.
+    }
+}
