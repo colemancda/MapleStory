e2a76ab80900fe0216f5cfc313eb9129f264dd11
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 22:02:34 -0400

    Update `UseInnerPortalRequest`

--- Sources/MapleStory62/Packets/UseInnerPortalRequest.swift
+++ Sources/MapleStory62/Packets/UseInnerPortalRequest.swift
@@ -7,9 +7,42 @@
 
 import Foundation
 
+/// Inner portal usage request packet.
+///
+/// **Note**: This packet is intentionally ignored by the server (no-op handler).
+/// It was designed for anti-cheat detection but was never fully implemented.
+/// The server currently does not validate inner portal usage.
 public struct UseInnerPortalRequest: MapleStoryPacket, Codable, Equatable, Hashable, Sendable {
 
     public static var opcode: ClientOpcode { .useInnerPortal }
 
-    public init() { }
+    /// Unknown flag byte
+    public let flag: UInt8
+
+    /// Name of the portal being used
+    public let portalName: String
+
+    /// Target X position (where player wants to teleport)
+    public let targetX: Int16
+
+    /// Target Y position (where player wants to teleport)
+    public let targetY: Int16
+
+    /// Current X position (where player is currently)
+    public let currentX: Int16
+
+    /// Current Y position (where player is currently)
+    public let currentY: Int16
+}
+
+extension UseInnerPortalRequest: MapleStoryDecodable {
+
+    public init(from container: MapleStoryDecodingContainer) throws {
+        self.flag = try container.decode(UInt8.self)
+        self.portalName = try container.decode(String.self)
+        self.targetX = try container.decode(Int16.self)
+        self.targetY = try container.decode(Int16.self)
+        self.currentX = try container.decode(Int16.self)
+        self.currentY = try container.decode(Int16.self)
+    }
 }
