a7939c63df740b56982018f86d7d5b7c62a883c6
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-24 21:36:55 -0400

    Implement SpousechatNotification payload

--- Sources/MapleStory62/Packets/SpousechatNotification.swift
+++ Sources/MapleStory62/Packets/SpousechatNotification.swift
@@ -9,6 +9,20 @@ public struct SpousechatNotification: MapleStoryPacket, Equatable, Hashable, Sen
 
     public static var opcode: ServerOpcode { .spousechat }
 
-    public init() { }
+    public let sender: String
+
+    public let message: String
+
+    public init(sender: String, message: String) {
+        self.sender = sender
+        self.message = message
+    }
 }
 
+extension SpousechatNotification: MapleStoryEncodable {
+
+    public func encode(to container: MapleStoryEncodingContainer) throws {
+        try container.encodeMapleAsciiString(sender)
+        try container.encodeMapleAsciiString(message)
+    }
+}
