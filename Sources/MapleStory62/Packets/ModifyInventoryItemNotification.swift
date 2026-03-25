2648afcf4193df8966c1eedc2bc1cc79b4e95c22
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-25 12:00:55 -0400

    Update `ModifyInventoryItemNotification`

--- Sources/MapleStory62/Packets/ModifyInventoryItemNotification.swift
+++ Sources/MapleStory62/Packets/ModifyInventoryItemNotification.swift
@@ -4,6 +4,7 @@
 //
 
 import Foundation
+import MapleStory
 
 public struct ModifyInventoryItemNotification: MapleStoryPacket, Equatable, Hashable, Sendable {
 
@@ -19,13 +20,15 @@ public struct ModifyInventoryItemNotification: MapleStoryPacket, Equatable, Hash
     public let mode: Mode
     public let inventoryType: InventoryType
     public let slot: Int8
+    public let fromSlot: Int8?
     public let item: InventoryItem?
     public let quantity: UInt16?
 
-    public init(mode: Mode, inventoryType: InventoryType, slot: Int8, item: InventoryItem? = nil, quantity: UInt16? = nil) {
+    public init(mode: Mode, inventoryType: InventoryType, slot: Int8, fromSlot: Int8? = nil, item: InventoryItem? = nil, quantity: UInt16? = nil) {
         self.mode = mode
         self.inventoryType = inventoryType
         self.slot = slot
+        self.fromSlot = fromSlot
         self.item = item
         self.quantity = quantity
     }
@@ -37,14 +40,13 @@ public struct ModifyInventoryItemNotification: MapleStoryPacket, Equatable, Hash
         toSlot: Int8,
         inventoryType: InventoryType
     ) -> ModifyInventoryItemNotification {
-        // In the protocol, move operations send the old slot as the value
-        // We'll encode this properly when the packet encoding is implemented
         var movedItem = item
         movedItem.slot = toSlot
         return ModifyInventoryItemNotification(
             mode: .move,
             inventoryType: inventoryType,
             slot: toSlot,
+            fromSlot: fromSlot,
             item: movedItem,
             quantity: nil
         )
@@ -96,3 +98,149 @@ public struct ModifyInventoryItemNotification: MapleStoryPacket, Equatable, Hash
     }
 }
 
+extension ModifyInventoryItemNotification: MapleStoryEncodable {
+    
+    public func encode(to encoder: Encoder) throws {
+        // The MapleStoryEncoder has special handling for MapleStoryEncodable types
+        // It will check the protocol and call encode(to: MapleStoryEncodingContainer)
+        // This method is only here to satisfy the Encodable protocol requirement
+        // and should not be called during normal packet encoding
+        fatalError("This should not be called. Use encode(to: MapleStoryEncodingContainer)")
+    }
+    
+    public func encode(to container: MapleStoryEncodingContainer) throws {
+        switch mode {
+        case .add:
+            try encodeAdd(to: container)
+        case .update:
+            try encodeUpdate(to: container)
+        case .move:
+            try encodeMove(to: container)
+        case .remove:
+            try encodeRemove(to: container)
+        }
+    }
+    
+    /// Encode add mode: fromDrop + 0x01 0x00 + inventoryType + slot + itemInfo
+    private func encodeAdd(to container: MapleStoryEncodingContainer) throws {
+        // fromDrop (assume false for now)
+        try container.encode(UInt8(0))
+        // operation mode bytes: 0x01 0x00
+        try container.encode(UInt8(1))
+        try container.encode(UInt8(0))
+        // inventory type
+        try container.encode(inventoryType.rawValue)
+        // slot
+        try container.encode(slot)
+        // item info
+        guard let item = item else {
+            throw EncodingError.invalidValue(
+                self,
+                .init(codingPath: container.codingPath, debugDescription: "Add mode requires item")
+            )
+        }
+        try encodeItemInfo(item, to: container)
+    }
+    
+    /// Encode update mode: fromDrop + 0x01 0x01 + inventoryType + slot + 0x00 + quantity
+    private func encodeUpdate(to container: MapleStoryEncodingContainer) throws {
+        // fromDrop (assume false for now)
+        try container.encode(UInt8(0))
+        // operation mode bytes: 0x01 0x01
+        try container.encode(UInt8(1))
+        try container.encode(UInt8(1))
+        // inventory type
+        try container.encode(inventoryType.rawValue)
+        // slot
+        try container.encode(slot)
+        // unknown byte
+        try container.encode(UInt8(0))
+        // quantity
+        guard let quantity = quantity else {
+            throw EncodingError.invalidValue(
+                self,
+                .init(codingPath: container.codingPath, debugDescription: "Update mode requires quantity")
+            )
+        }
+        try container.encode(quantity)
+    }
+    
+    /// Encode move mode: 0x01 0x01 0x02 + inventoryType + src(short) + dst(short)
+    private func encodeMove(to container: MapleStoryEncodingContainer) throws {
+        // operation mode bytes: 0x01 0x01 0x02
+        try container.encode(UInt8(1))
+        try container.encode(UInt8(1))
+        try container.encode(UInt8(2))
+        // inventory type
+        try container.encode(inventoryType.rawValue)
+        // source slot (as short)
+        guard let fromSlot = fromSlot else {
+            throw EncodingError.invalidValue(
+                self,
+                .init(codingPath: container.codingPath, debugDescription: "Move mode requires fromSlot")
+            )
+        }
+        try container.encode(Int16(fromSlot))
+        // destination slot (as short)
+        try container.encode(Int16(slot))
+    }
+    
+    /// Encode remove mode: fromDrop + 0x01 0x03 + inventoryType + slot(short)
+    private func encodeRemove(to container: MapleStoryEncodingContainer) throws {
+        // fromDrop (assume false for now)
+        try container.encode(UInt8(0))
+        // operation mode bytes: 0x01 0x03
+        try container.encode(UInt8(1))
+        try container.encode(UInt8(3))
+        // inventory type
+        try container.encode(inventoryType.rawValue)
+        // slot (as short)
+        try container.encode(Int16(slot))
+    }
+    
+    /// Encode item info (simplified version for non-equipment, non-pet items)
+    private func encodeItemInfo(_ item: InventoryItem, to container: MapleStoryEncodingContainer) throws {
+        // Position
+        try container.encode(item.slot)
+        
+        // Item type (1 = equip, 2 = use, 3 = setup, 4 = etc, 5 = cash)
+        let itemType: UInt8
+        if item.equip != nil {
+            itemType = 1
+        } else {
+            // Simple assumption based on inventory type
+            itemType = inventoryType.rawValue
+        }
+        try container.encode(itemType)
+        
+        // Item ID
+        try container.encode(item.itemId, isLittleEndian: true)
+        
+        // For equipment, encode stats
+        if let equipData = item.equip {
+            try encodeEquipData(equipData, to: container)
+        }
+    }
+    
+    /// Encode equipment data
+    private func encodeEquipData(_ equip: EquipData, to container: MapleStoryEncodingContainer) throws {
+        try container.encode(Int16(equip.slots))
+        try container.encode(UInt8(0)) // ?
+        try container.encode(equip.str)
+        try container.encode(equip.dex)
+        try container.encode(equip.int)
+        try container.encode(equip.luk)
+        try container.encode(equip.hp)
+        try container.encode(equip.mp)
+        try container.encode(equip.weaponAttack)
+        try container.encode(equip.magicAttack)
+        try container.encode(equip.weaponDefense)
+        try container.encode(equip.magicDefense)
+        try container.encode(equip.accuracy)
+        try container.encode(equip.avoid)
+        try container.encode(equip.speed)
+        try container.encode(equip.jump)
+        try container.encode(equip.owner ?? "", fixedLength: 13)
+        try container.encode(UInt8(0)) // ?
+    }
+}
