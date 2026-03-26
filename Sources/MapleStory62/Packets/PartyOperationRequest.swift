e28e3a33ed0b96efb1132233ef4dc3bbd11520b5
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-26 00:32:21 -0400

    Update `PartyOperationRequest`

--- Sources/MapleStory62/Packets/PartyOperationRequest.swift
+++ Sources/MapleStory62/Packets/PartyOperationRequest.swift
@@ -6,59 +6,77 @@
 //
 
 import Foundation
+import MapleStory
 
-public struct PartyOperationRequest: MapleStoryPacket, Equatable, Hashable, Sendable {
+/// Party operation request from the client.
+///
+/// Each case represents a different party operation with its associated data:
+/// - `create`: Create a new party
+/// - `leave`: Leave current party
+/// - `accept`: Accept a party invitation with the party ID
+/// - `invite`: Invite a player by character name
+/// - `expel`: Kick a member (leader only) by character ID
+/// - `passLeader`: Transfer leadership to another member by character ID
+/// - `disband`: Disband the party (leader only)
+public enum PartyOperationRequest: MapleStoryPacket, Equatable, Hashable, Sendable {
 
     public static var opcode: ClientOpcode { .partyOperation }
 
-    public let operation: UInt8
+    /// Create a new party
+    case create
 
-    /// Used by operation `0x03` (accept invite).
-    public let partyID: PartyID?
+    /// Leave current party
+    case leave
 
-    /// Used by operation `0x04` (invite by character name).
-    public let invitedName: String?
+    /// Accept a party invitation
+    /// - Parameter partyID: The ID of the party to join
+    case accept(partyID: PartyID)
 
-    /// Used by operations `0x05` (expel) and `0x06` (pass leader).
-    public let targetCharacterID: Character.ID?
+    /// Invite a player to the party
+    /// - Parameter characterName: The name of the character to invite
+    case invite(characterName: String)
+
+    /// Expel a member from the party (leader only)
+    /// - Parameter characterID: The ID of the character to expel
+    case expel(characterID: Character.ID)
+
+    /// Transfer party leadership to another member
+    /// - Parameter characterID: The ID of the new leader
+    case passLeader(characterID: Character.ID)
+
+    /// Disband the party (leader only)
+    case disband
 }
 
 extension PartyOperationRequest: MapleStoryDecodable {
 
     public init(from container: MapleStoryDecodingContainer) throws {
-        self.operation = try container.decode(UInt8.self)
+        let operation = try container.decode(UInt8.self)
         switch operation {
+        case 0x01:
+            self = .create
+        case 0x02:
+            self = .leave
         case 0x03:
             // Accept invite
-            if container.remainingBytes >= 4 {
-                self.partyID = try container.decode(PartyID.self)
-            } else {
-                self.partyID = nil
-            }
-            self.invitedName = nil
-            self.targetCharacterID = nil
+            let partyID = try container.decode(PartyID.self)
+            self = .accept(partyID: partyID)
         case 0x04:
             // Invite
-            self.partyID = nil
-            if container.remainingBytes > 0 {
-                self.invitedName = try container.decode(String.self)
-            } else {
-                self.invitedName = nil
-            }
-            self.targetCharacterID = nil
-        case 0x05, 0x06:
-            // Expel / pass leader
-            self.partyID = nil
-            self.invitedName = nil
-            if container.remainingBytes >= 4 {
-                self.targetCharacterID = try container.decode(Character.ID.self)
-            } else {
-                self.targetCharacterID = nil
-            }
+            let name = try container.decode(String.self)
+            self = .invite(characterName: name)
+        case 0x05:
+            // Expel member
+            let characterID = try container.decode(Character.ID.self)
+            self = .expel(characterID: characterID)
+        case 0x06:
+            // Pass leadership
+            let characterID = try container.decode(Character.ID.self)
+            self = .passLeader(characterID: characterID)
+        case 0x07:
+            self = .disband
         default:
-            self.partyID = nil
-            self.invitedName = nil
-            self.targetCharacterID = nil
+            throw DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: "Unknown party operation: \(operation)"))
         }
     }
 }
