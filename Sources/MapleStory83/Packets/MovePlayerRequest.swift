7339dcde2ffae8b31e9f5516efa1214c50b03913
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-28 00:03:02 -0400

    Add v83 `MovePlayerRequest`

---
+++ Sources/MapleStory83/Packets/MovePlayerRequest.swift
@@ -0,0 +1,22 @@
+//
+//  MovePlayerRequest.swift
+//
+
+import Foundation
+
+public struct MovePlayerRequest: MapleStoryPacket, Codable, Equatable, Hashable, Sendable {
+
+    public static var opcode: ClientOpcode { .movePlayer }
+
+    internal let value0: UInt8
+
+    internal let value1: UInt32
+
+    public let movements: [Movement]
+
+    internal let value2: UInt64
+
+    internal let value3: UInt64
+
+    internal let value4: UInt16
+}
