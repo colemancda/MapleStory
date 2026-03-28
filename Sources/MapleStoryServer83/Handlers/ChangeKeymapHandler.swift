fa07e84f778192f969c7e76634cc4feee11da8be
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-28 08:30:45 -0400

    Add v83 `ChangeKeymapHandler`

---
+++ Sources/MapleStoryServer83/Handlers/ChangeKeymapHandler.swift
@@ -0,0 +1,31 @@
+//
+//  ChangeKeymapHandler.swift
+//
+
+import Foundation
+import CoreModel
+import MapleStory
+import MapleStory83
+import MapleStoryServer
+
+public struct ChangeKeymapHandler: PacketHandler {
+
+    public typealias Packet = MapleStory83.ChangeKeymapRequest
+
+    public init() { }
+
+    public func handle<Socket: MapleStorySocket, Database: ModelStorage>(
+        packet: Packet,
+        connection: MapleStoryServer<Socket, Database, ClientOpcode, ServerOpcode>.Connection
+    ) async throws {
+        guard let character = try await connection.character else { return }
+
+        let keymap = packet.bindings.map { binding in
+            KeymapEntry(key: binding.key, type: binding.type, action: binding.action)
+        }
+
+        await KeymapRegistry.shared.saveKeymap(keymap, for: character.id)
+
+        try await KeymapRegistry.shared.saveKeymap(for: character.id, database: connection.database)
+    }
+}
