c5592acb0ced4c6cfa2bfce40ffa5267d4341d70
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-28 00:20:28 -0400

    Add v83 `RangedAttackNotification`

---
+++ Sources/MapleStory83/Packets/RangedAttackNotification.swift
@@ -0,0 +1,47 @@
+//
+//  RangedAttackNotification.swift
+//
+
+import Foundation
+
+/// Ranged (bow/gun) attack broadcast to nearby players.
+///
+public struct RangedAttackNotification: MapleStoryPacket, Codable, Equatable, Hashable, Sendable {
+
+    public static var opcode: ServerOpcode { .rangedAttack }
+
+    public let characterID: UInt32
+
+    public let numAttackedAndDamage: UInt8
+
+    public let unknown: UInt8
+
+    public let skillLevel: UInt8
+
+    public let skillID: UInt32?
+
+    public let display: UInt8
+
+    public let direction: UInt8
+
+    public let stance: UInt8
+
+    public let speed: UInt8
+
+    public let unknown2: UInt8
+
+    public let projectile: UInt32
+
+    public let targets: [AttackTarget]
+
+    public let unknown3: UInt32
+
+    public struct AttackTarget: Codable, Equatable, Hashable, Sendable {
+
+        public let monsterObjectID: UInt32
+
+        public let unknown: UInt8
+
+        public let damageLines: [Int32]
+    }
+}
