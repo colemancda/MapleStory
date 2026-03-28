f5ea7d147aac889768e136bdac99b8f48fd95d03
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-28 00:03:05 -0400

    Add v83 `CouponCodeRequest`

---
+++ Sources/MapleStory83/Packets/CouponCodeRequest.swift
@@ -0,0 +1,20 @@
+//
+//  CouponCodeRequest.swift
+//
+
+import Foundation
+
+public struct CouponCodeRequest: MapleStoryPacket, Equatable, Hashable, Sendable {
+
+    public static var opcode: ClientOpcode { .couponCode }
+
+    public let code: String
+}
+
+extension CouponCodeRequest: MapleStoryDecodable {
+
+    public init(from container: MapleStoryDecodingContainer) throws {
+        let _ = try container.decode(UInt16.self)
+        self.code = try container.decode(String.self)
+    }
+}
