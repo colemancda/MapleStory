2826f03def97147c5a27de77ed0ebedb4cf938c5
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-28 00:03:04 -0400

    Add v83 `DamageSummonRequest`

---
+++ Sources/MapleStory83/Packets/DamageSummonRequest.swift
@@ -0,0 +1,20 @@
+//
+//  DamageSummonRequest.swift
+//
+
+import Foundation
+
+public struct DamageSummonRequest: MapleStoryPacket, Codable, Equatable, Hashable, Sendable {
+
+    public static var opcode: ClientOpcode { .damageSummon }
+
+    internal let value0: UInt32
+
+    public let unkByte: UInt8
+
+    public let damage: UInt32
+
+    public let monsterIDFrom: UInt32
+
+    public let stance: UInt8
+}
