b93fc9eb114da5fab9b8ac0ad23d7583cd5d3ae6
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-28 00:03:04 -0400

    Add v83 `ItemMoveRequest`

---
+++ Sources/MapleStory83/Packets/ItemMoveRequest.swift
@@ -0,0 +1,24 @@
+//
+//  ItemMoveRequest.swift
+//
+
+import Foundation
+
+public struct ItemMoveRequest: MapleStoryPacket, Codable, Equatable, Hashable, Sendable {
+
+    public static var opcode: ClientOpcode { .itemMove }
+
+    internal let value0: UInt32
+
+    /// Inventory type (1=equip, 2=use, 3=setup, 4=etc, 5=cash)
+    public let inventoryType: UInt8
+
+    /// Source slot
+    public let sourceSlot: Int16
+
+    /// Destination slot (negative = equip slot)
+    public let destinationSlot: Int16
+
+    /// Quantity to move
+    public let quantity: Int16
+}
