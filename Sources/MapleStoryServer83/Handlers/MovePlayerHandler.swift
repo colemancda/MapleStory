698aad089cc87e43ceb088b5a229bf4b1f62329a
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-28 08:20:19 -0400

    Add v83 `MovePlayerHandler`

---
+++ Sources/MapleStoryServer83/Handlers/MovePlayerHandler.swift
@@ -0,0 +1,35 @@
+//
+//  MovePlayerHandler.swift
+//
+
+import Foundation
+import CoreModel
+import MapleStory
+import MapleStory83
+import MapleStoryServer
+
+public struct MovePlayerHandler: PacketHandler {
+
+    public typealias Packet = MapleStory83.MovePlayerRequest
+
+    public init() { }
+
+    public func handle<Socket: MapleStorySocket, Database: ModelStorage>(
+        packet: Packet,
+        connection: MapleStoryServer<Socket, Database, ClientOpcode, ServerOpcode>.Connection
+    ) async throws {
+        guard let character = try await connection.character else { return }
+
+        if let finalPos = packet.movements.finalPosition {
+            let playerPos = PlayerPosition(x: finalPos.x, y: finalPos.y)
+            await PlayerPositionRegistry.shared.updatePosition(playerPos, for: character.id)
+        }
+
+        let notification = MovePlayerNotification(
+            characterID: character.index,
+            unknown: 0,
+            movements: packet.movements
+        )
+        try await connection.broadcast(notification, map: character.currentMap)
+    }
+}
