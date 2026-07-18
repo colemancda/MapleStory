7494aa16eb2f894ba24931bb0a302448634a2b06
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-18 02:00:33 -0400

    Migrate Storage to CoreModel ModelData entity coding

--- Sources/MapleStory/Model/Storage.swift
+++ Sources/MapleStory/Model/Storage.swift
@@ -72,7 +72,33 @@ public struct Storage: Codable, Equatable, Hashable, Identifiable, Sendable {
 // MARK: - Entity
 
 extension Storage: Entity {
-    
+
+    public init(from container: ModelData) throws {
+        guard container.entity.rawValue == Self.entityName.rawValue else {
+            throw CoreModel.CoreModelError.invalidEntity(container.entity)
+        }
+        guard let id = Self.ID(objectID: container.id) else {
+            throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: [], debugDescription: "Cannot decode identifier from \(container.id)"))
+        }
+        self.id = id
+        self.mesos = try container.decode(UInt32.self, forKey: Storage.CodingKeys.mesos)
+        self.maxSlots = try container.decode(UInt8.self, forKey: Storage.CodingKeys.maxSlots)
+        self.items = try container.decode([Int8: InventoryItem].self, forKey: Storage.CodingKeys.items)
+        self.userID = try container.decodeRelationship(User.ID.self, forKey: Storage.CodingKeys.userID)
+    }
+
+    public func encode() -> ModelData {
+        var container = ModelData(
+            entity: Self.entityName,
+            id: ObjectID(self.id)
+        )
+        container.encode(self.mesos, forKey: Storage.CodingKeys.mesos)
+        container.encode(self.maxSlots, forKey: Storage.CodingKeys.maxSlots)
+        container.encode(self.items, forKey: Storage.CodingKeys.items)
+        container.encodeRelationship(self.userID, forKey: Storage.CodingKeys.userID)
+        return container
+    }
+
     public static var attributes: [CodingKeys: AttributeType] {
         [
             .mesos: .int64,
